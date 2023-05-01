47d8c614832ff831cbbd3109bd4c5175639b1266
Farzad <farzad.rabiee@outlook.com>
2023-05-01 20:23:28 +1000

    Add hover on each cell Install UUID package

--- src/components/table/TableCols.tsx
+++ src/components/table/TableCols.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import {IRowCol} from "./IRowCol";
+import {v4 as uuidv4} from 'uuid';
 
 const TableCols = ({rowCol}: { rowCol: IRowCol }) => {
     const cols: Array<number> = [];
     for (let i = 0; i < rowCol.col; i++)
         cols.push(i);
 
+    const mouseHover = (uuid: string) => {
+        console.info(uuid);
+    }
+
     return (
         <>
             {
                 cols.map(c =>
-                    <td key={c} className="text-l p-4 border-2 border-amber-600">{c}</td>
+                    <td key={uuidv4()}
+                        className="select-none text-l p-4 border-2 border-amber-600 hover:backdrop-blur-xl hover:border-amber-500"
+                        onMouseOver={() => mouseHover(uuidv4())}>{c}</td>
                 )
             }
         </>
