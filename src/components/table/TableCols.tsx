60cd8ab5da391d5740a7e5afbeb90c6d0dfaa40e
Farzad <farzad.rabiee@outlook.com>
2023-05-01 20:48:57 +1000

    Save the clicked cells in an array Install Lodash package

--- src/components/table/TableCols.tsx
+++ src/components/table/TableCols.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {IRowCol} from "./IRowCol";
 import {v4 as uuidv4} from 'uuid';
+import _ from 'lodash';
 
 interface colUuid {
     col: number;
@@ -8,14 +9,25 @@ interface colUuid {
 }
 
 const TableCols = ({rowCol}: { rowCol: IRowCol }) => {
+    const [selectedCells, setSelectedCells] = useState<Array<string>>([]);
+
     const cols: Array<colUuid> = [];
     for (let i = 0; i < rowCol.col; i++) {
         const uuid = uuidv4();
         cols.push({col: i, uuid: uuid});
     }
 
-    const mouseHover = (uuid: string) => {
-        console.info(uuid);
+    const mouseHover = () => {
+        console.info('Hover!');
+    }
+
+    const mouseClick = (uuid: string) => {
+        console.log(_.includes(selectedCells, uuid))
+        if (!_.includes(selectedCells, uuid)) {
+            setSelectedCells([...selectedCells, uuid]);
+        }
+
+        console.info({selectedCells});
     }
 
     return (
@@ -24,7 +36,7 @@ const TableCols = ({rowCol}: { rowCol: IRowCol }) => {
                 cols.map(c =>
                     <td key={c.uuid}
                         className="select-none text-l p-4 border-2 border-amber-600 hover:backdrop-blur-xl hover:border-amber-500"
-                        onMouseOver={() => mouseHover(c.uuid)}>{c.uuid}</td>
+                        onMouseOver={() => mouseHover} onClick={() => mouseClick(c.uuid)}>{c.uuid}</td>
                 )
             }
         </>
