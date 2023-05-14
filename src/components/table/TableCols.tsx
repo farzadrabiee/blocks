1199b8a25975974e9e8d670a2aa8268715a41379
Farzad <farzad.rabiee@outlook.com>
2023-05-14 20:12:13 +1000

    Reformat code

--- src/components/table/TableCols.tsx
+++ src/components/table/TableCols.tsx
@@ -1,44 +1,47 @@
-import React, {useState} from 'react';
-import {IRowCol} from "./IRowCol";
-import {v4 as uuidv4} from 'uuid';
+import React from 'react';
+import { IRowCol } from "./IRowCol";
+import { v4 as uuidv4 } from 'uuid';
 import _ from 'lodash';
 
-interface colUuid {
+interface IColUuid {
     col: number;
     uuid: string;
 }
 
-const TableCols = ({rowCol}: { rowCol: IRowCol }) => {
-    const [selectedCells, setSelectedCells] = useState<Array<string>>([]);
+const TableCols = ({ rowCol }: { rowCol: IRowCol }) => {
+    // const [selectedCells, setSelectedCells] = useState<Array<string>>([]);
 
-    const cols: Array<colUuid> = [];
-    for (let i = 0; i < rowCol.col; i++) {
-        const uuid = uuidv4();
-        cols.push({col: i, uuid: uuid});
-    }
+    const cols: IColUuid[] = Array.from({ length: rowCol.col }, (_, i) => ({
+        col: i,
+        uuid: uuidv4(),
+    }));
 
-    const mouseHover = () => {
-        console.info('Hover!');
-    }
+    const mouseHover = (e: React.MouseEvent<HTMLTableDataCellElement>) => {
+        console.log(`Hover on ${e.currentTarget.dataset.key}`);
+    };
 
-    const mouseClick = (uuid: string) => {
-        console.log(_.includes(selectedCells, uuid))
-        if (!_.includes(selectedCells, uuid)) {
-            setSelectedCells([...selectedCells, uuid]);
-        }
+    const mouseClick = () => {
+        console.log('Click!')
+        // const selected = [...selectedCells, uuid];
+        // // selected = _.uniq(selected);
+        // setSelectedCells(selected);
 
-        console.info({selectedCells});
+        // console.info({selectedCells});
     }
 
     return (
         <>
-            {
-                cols.map(c =>
-                    <td key={c.uuid}
-                        className="select-none text-l p-4 border-2 border-amber-600 hover:backdrop-blur-xl hover:border-amber-500"
-                        onMouseOver={() => mouseHover} onClick={() => mouseClick(c.uuid)}>{c.uuid}</td>
-                )
-            }
+            {cols.map((c) => (
+                <td
+                    key={c.uuid}
+                    className="select-none text-l p-4 border-2 border-amber-600 hover:backdrop-blur-xl hover:border-amber-500"
+                    data-key={c.uuid}
+                    onMouseOver={mouseHover}
+                    onClick={mouseClick}
+                >
+                    {c.uuid}
+                </td>
+            ))}
         </>
     )
 }
--- src/components/table/TableRows.tsx
+++ src/components/table/TableRows.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import {IRowCol} from "./IRowCol";
+import React from "react";
+import { IRowCol } from "./IRowCol";
 import TableCols from "./TableCols";
 
-const TableRows = ({rowCol}: { rowCol: IRowCol }) => {
-    const rows: Array<number> = [];
-    for (let i = 0; i < rowCol.row; i++)
-        rows.push(i);
-
+const TableRows = ({ rowCol }: { rowCol: IRowCol }) => {
     return (
         <>
-            {
-                rows.map(r =>
-                    <tr key={r}>
-                        <TableCols rowCol={rowCol}/>
-                    </tr>
-                )
-            }
+            {Array.from({ length: rowCol.row }).map((_, r) => (
+                <tr key={r}>
+                    <TableCols rowCol={rowCol} />
+                </tr>
+            ))}
         </>
-    )
+    );
 }
 
 export default TableRows;
